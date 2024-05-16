1263bf537b89003bc99b3d8a8ddd1354c7cfaa71
Sreya Goswami <47354994+doped-semiconductor@users.noreply.github.com>
2024-05-16 22:08:16 +0530

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS conversedb;
-USE conversedb;
 CREATE USER 'converse-sql-bot'@'localhost' IDENTIFIED BY 'converse123';
 GRANT ALL PRIVILEGES ON conversedb.* TO 'converse-sql-bot'@'localhost';
+USE conversedb;
+
