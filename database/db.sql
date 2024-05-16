fc3d6d4fdd56f044f27cd91226433415c9efb261
Sreya Goswami <getsreya@gmail.com>
2024-05-16 21:25:57 +0530

    DB User Setup

--- database/db.sql
+++ database/db.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE IF NOT EXISTS conversedb;
 USE conversedb;
+CREATE USER 'converse-sql-bot'@'localhost' IDENTIFIED BY 'converse123';
+GRANT ALL PRIVILEGES ON conversedb.* TO 'converse-sql-bot'@'localhost';
